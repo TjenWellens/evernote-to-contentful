97f76fda9334eaf984d23dc2c37d5fcac31f3b79
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-08 13:34:29 +0100

    lib.evernote.findUpdates.js

---
+++ lib.evernote.findUpdates.js
@@ -0,0 +1,85 @@
+const {NoteStore} = require("evernote");
+const {createEvernoteClient} = require("./lib.evernote");
+
+const noteStore = createEvernoteClient().getNoteStore();
+
+async function* getNotesGenerator(notebook, preferredPageSize = 100) {
+	const filter = new NoteStore.NoteFilter({
+		notebookGuid: notebook.guid,
+	});
+	const spec = new NoteStore.NotesMetadataResultSpec({
+		includeUpdated: true,
+		includeTitle: false,
+		includeContentLength: false,
+		includeCreated: false,
+		includeDeleted: false,
+		includeUpdateSequenceNum: false,
+		includeNotebookGuid: false,
+		includeTagGuids: false,
+		includeAttributes: false,
+		includeLargestResourceMime: false,
+		includeLargestResourceSize: false,
+	});
+
+	let page = 0
+	let res
+	do {
+		res = await noteStore.findNotesMetadata(filter, page * entryCount(), preferredPageSize, spec)
+		for (const entry of res.notes) {
+			yield entry
+		}
+		page += 1
+	} while (res.totalNotes > entryCount())
+
+	function entryCount() {
+		if(!res) return 0
+		return res.startIndex + res.notes.length
+	}
+}
+
+async function findUpdatedNotes(notebook, query = {}) {
+	const filter = new NoteStore.NoteFilter({
+		...query,
+		notebookGuid: notebook.guid,
+	});
+	const spec = new NoteStore.NotesMetadataResultSpec({
+		includeUpdated: true,
+		includeTitle: false,
+		includeContentLength: false,
+		includeCreated: false,
+		includeDeleted: false,
+		includeUpdateSequenceNum: false,
+		includeNotebookGuid: false,
+		includeTagGuids: false,
+		includeAttributes: false,
+		includeLargestResourceMime: false,
+		includeLargestResourceSize: false,
+	});
+
+	const offset = 0;
+	const limitPerPage = 500;
+	const res = await noteStore.findNotesMetadata(filter, offset, limitPerPage, spec)
+
+	return res.notes
+}
+
+async function toArray(promisedGenerator) {
+	const result = []
+	for await (const entry of promisedGenerator) {
+		result.push(entry)
+	}
+	return result
+}
+
+async function getNotesUpdateDictionary(notebook) {
+	const notes = await toArray(getNotesGenerator(notebook));
+	return notes
+		.reduce((result, note) => ({
+			...result,
+			[note.guid]: note.updated
+		}), {})
+}
+
+module.exports = {
+	getNotesUpdateDictionary,
+}
