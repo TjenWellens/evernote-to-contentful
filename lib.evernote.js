52cd1a53662c2882155a61ca40333fb322579e42
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-08 19:08:25 +0100

    evernote client work with prod env

--- lib.evernote.js
+++ lib.evernote.js
@@ -1,7 +1,11 @@
 const evernote = require('evernote');
 
 function createEvernoteClient() {
-	return new evernote.Client({token: process.env.EVERNOTE_TOKEN});
+	return new evernote.Client({
+		token: process.env.EVERNOTE_TOKEN,
+		sandbox: process.env.EVERNOTE_IS_SANDBOX === 'true',
+		china: false,
+	});
 }
 
 module.exports = {
