adedef8661f83eec67f89c05dc6a8f77d72a1832
Tjen Wellens <tjen.wellens@gmail.com>
2020-12-10 18:01:42 +0100

    refactor

--- lib.content2content.yankImageToRoot.js
+++ lib.content2content.yankImageToRoot.js
@@ -2,8 +2,12 @@ function isImage(entry) {
 	return entry.nodeType === "embedded-asset-block";
 }
 
+function hasChildren(entry) {
+	return entry.content;
+}
+
 function yankImageToRoot(entry) {
-	if (entry.content && isImage(entry.content[0]))
+	if (hasChildren(entry) && isImage(entry.content[0]))
 		return entry.content[0]
 	return entry
 }
