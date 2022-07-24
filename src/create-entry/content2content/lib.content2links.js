d4edd5f4e9f57892e367cdaf6b658d14fa0231b4
Tjen Wellens <tjen.wellens@gmail.com>
2022-07-24 16:41:23 +0100

    content2links: green

--- src/create-entry/content2content/lib.content2links.js
+++ src/create-entry/content2content/lib.content2links.js
@@ -1,8 +1,32 @@
 const {parseXmlToJs} = require("./evernote-xml-to-js-parser");
 
+function hasChildren(node) {
+    return node.$$ && node.$$.length !== 0
+}
+
+function isLink(node) {
+    return node["#name"] === "a"
+}
+
+function getUrlFromLinkNode(node) {
+    return node.$.href
+}
+
+function extractLinks(node) {
+    if(isLink(node)) {
+        return getUrlFromLinkNode(node)
+    }
+
+    if(hasChildren(node)) {
+        return node.$$.flatMap(extractLinks)
+    }
+
+    return []
+}
+
 async function content2links(noteContent) {
     const content = await parseXmlToJs(noteContent)
-    return []
+    return content.$$.flatMap(extractLinks)
 }
 
 module.exports = {
