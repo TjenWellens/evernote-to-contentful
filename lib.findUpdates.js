a5bc05be8e6d0e7fd0a328dce0a2afbf77847f9c
Tjen Wellens <tjen.wellens@gmail.com>
2022-07-24 11:44:49 +0100

    refactor: move to ext/contentful

--- lib.findUpdates.js
+++ lib.findUpdates.js
@@ -1,11 +1,11 @@
 const {compareTagUpdates} = require("./lib.compareUpdates");
-const {getTagUpdates: getContentfulTagUpdates} = require("./lib.contentful.getTagUpdates");
+const {getTagUpdates: getContentfulTagUpdates} = require("./ext/contentful/lib.contentful.getTagUpdates");
 const {getTagUpdates: getEvernoteTagUpdates} = require("./lib.evernote.getTagUpdates");
 
 const {compareNoteUpdates} = require("./lib.compareUpdates");
 
 const {getNotesUpdates} = require("./lib.evernote.findNoteUpdates");
-const {getBlogUpdates} = require("./lib.contentful.getBlogUpdates");
+const {getBlogUpdates} = require("./ext/contentful/lib.contentful.getBlogUpdates");
 
 async function findUpdatedNoteIds(notebook) {
 	const notes = await getNotesUpdates(notebook)
